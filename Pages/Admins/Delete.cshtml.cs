13efffe57dd97f95af3970b0fcc5ed4c8a560024
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-01-25 16:39:21 +0100

    Admin. Added delete through repository

--- Pages/Admins/Delete.cshtml.cs
+++ Pages/Admins/Delete.cshtml.cs
@@ -12,24 +12,32 @@ namespace FribergCarsRazor.Pages.Admins
 {
     public class DeleteModel : PageModel
     {
-        private readonly FribergCarsRazor.ApplicationDbContext _context;
+        //private readonly FribergCarsRazor.ApplicationDbContext _context;
 
-        public DeleteModel(FribergCarsRazor.ApplicationDbContext context)
+        //public DeleteModel(FribergCarsRazor.ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        private readonly IAdmin adminRepo;
+
+        public DeleteModel(IAdmin adminRepo)
         {
-            _context = context;
+            this.adminRepo = adminRepo;
         }
 
         [BindProperty]
         public Admin Admin { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var admin = await _context.Admins.FirstOrDefaultAsync(m => m.AdminId == id);
+            //var admin = await _context.Admins.FirstOrDefaultAsync(m => m.AdminId == id);
+            var admin = adminRepo.GetById(id);
 
             if (admin == null)
             {
@@ -42,19 +50,22 @@ namespace FribergCarsRazor.Pages.Admins
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync(int id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var admin = await _context.Admins.FindAsync(id);
+            //var admin = await _context.Admins.FindAsync(id);
+            var admin = adminRepo.GetById(id);
+
             if (admin != null)
             {
                 Admin = admin;
-                _context.Admins.Remove(Admin);
-                await _context.SaveChangesAsync();
+                //_context.Admins.Remove(Admin);
+                //await _context.SaveChangesAsync();
+                adminRepo.DeleteAdmin(admin);
             }
 
             return RedirectToPage("./Index");
