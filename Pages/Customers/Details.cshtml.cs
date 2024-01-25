81a07ef27bc0bb0be1218b2a94ea3e29f5403db3
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-01-25 15:45:26 +0100

    Details for customer working through repository.

--- Pages/Customers/Details.cshtml.cs
+++ Pages/Customers/Details.cshtml.cs
@@ -11,23 +11,30 @@ namespace FribergCarsRazor.Pages.Customers
 {
     public class DetailsModel : PageModel
     {
-        private readonly ApplicationDbContext _context;
+        //private readonly ApplicationDbContext _context;
 
-        public DetailsModel(ApplicationDbContext context)
+        //public DetailsModel(ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        private readonly ICustomer customerRepo;
+
+        public DetailsModel(ICustomer customerRepo)
         {
-            _context = context;
+            this.customerRepo = customerRepo;
         }
-
         public Customer Customer { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var customer = await _context.Customers.FirstOrDefaultAsync(m => m.CustomerId == id);
+            //var customer = await _context.Customers.FirstOrDefaultAsync(m => m.CustomerId == id);
+            var customer = customerRepo.GetById(id);
             if (customer == null)
             {
                 return NotFound();
