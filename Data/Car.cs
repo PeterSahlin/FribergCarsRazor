309c268c3243420576938b736187f89179b535a9
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-01-25 09:42:30 +0100

    Add project files.

---
+++ Data/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace FribergCarsRazor.Data
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string CarName { get; set; }
+        public string Seats { get; set; }
+
+        public int Price { get; set; }
+
+        public string? Image { get; set; }
+
+    }
+}
